8b1295d89ef9cd077f63f6a44d070d790c56d44d
AshRepartoFerramenta <dcapod07@hotmail.com>
2020-08-26 17:52:46 +0200

    Add class on index.php from new file

--- index.php
+++ index.php
@@ -1,23 +1,6 @@
 <?php
 
-class User
-{
-    public $name;
-    public $lastname;
-    public $email;
-
-    public function __construct($_nome, $_cognome, $_email)
-    {
-        $this->name = $_nome;
-        $this->lastname = $_cognome;
-        $this->email = $_email;
-    }
-
-    public function userData()
-    {
-        return $this->name . ' ' . $this->lastname . ' | ' . 'email: ' . $this->email . '<br>';
-    }
-};
+require(__DIR__.'/user.php');
 
 
 $utenti = [
---
+++ user.php
@@ -0,0 +1,22 @@
+<?php
+
+class User
+{
+    public $name;
+    public $lastname;
+    public $email;
+
+    public function __construct($_nome, $_cognome, $_email)
+    {
+        $this->name = $_nome;
+        $this->lastname = $_cognome;
+        $this->email = $_email;
+    }
+
+    public function userData()
+    {
+        return $this->name . ' ' . $this->lastname . ' | ' . 'email: ' . $this->email . '<br>';
+    }
+};
+
+?>
